a9d2af9168c48da27f9b3108d267accb144f1e51
LvLTroubleshooter <ismailliam144@gmail.com>
2024-12-08 14:12:25 +0800

    testing the .env addition

--- src/main/java/com/example/movies/MoviesApplication.java
+++ src/main/java/com/example/movies/MoviesApplication.java
@@ -3,8 +3,10 @@ package com.example.movies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MoviesApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(MoviesApplication.class, args);
